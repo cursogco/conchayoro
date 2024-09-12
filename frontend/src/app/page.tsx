c1d51b00ea1902ad63ae90450a421f7ea91e7992
cursogco <31414946+cursogco@users.noreply.github.com>
2024-09-12 11:17:05 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <div>
-        <h1> Welcome to the ConchaYOro App - deploy v2</h1>
+        <h1> Welcome to the ConchaYOro App - deploy v1</h1>
       </div>
   )
 }
