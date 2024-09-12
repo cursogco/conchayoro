a4d012851d5594e814319c957ce2c589ac36e5b1
cursogco <jps012009@yahoo.com.br>
2024-09-12 20:39:33 -0300

    Ajustes page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,13 @@ export default async function Page() {
   const black_friday = await blackFridayFlag();
 
   return (
-      <div>
-        <h1> Welcome to the ConchaYOro App - deploy v3a</h1>
-        { black_friday ? <button> Promo </button> : <button> Normal </button> } 
-      </div>
+    <html>
+      <body>
+        <div>
+          <h1> Welcome to the ConchaYOro App - deploy v3a</h1>
+          { black_friday ? <button> Promo </button> : <button> Normal </button> } 
+        </div>
+      </body>
+    </html>
   )
 };
