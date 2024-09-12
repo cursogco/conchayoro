0bdeb1b026e6073bcdcfd4a973a0f8beebf073b3
cursogco <31414946+cursogco@users.noreply.github.com>
2024-09-12 17:51:25 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
-import { blackFridayFlag } from '@/flags';
+import { blackFridayFlag } from '@/app/flags';
 
 export default async function Page() {
   const black_friday = await blackFridayFlag();
 
   return (
       <div>
-        <h1> Welcome to the ConchaYOro App - deploy v1</h1>
-        black_friday ? <button> Promo </button> : <button> Normal </button>
+        <h1> Welcome to the ConchaYOro App - deploy v3</h1>
+        { black_friday ? <button> Promo </button> : <button> Normal </button> } 
       </div>
   )
 };
